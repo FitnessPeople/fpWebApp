71d2ecec1fe9697d0a7f9c588a35ede688557b98
javiergalvanmancera <javier.galvan.mancera@gmail.com>
2025-02-10 11:14:52 -0500

    Otros metodos de clases globales

--- clasesglobales.cs
+++ clasesglobales.cs
@@ -14,6 +14,167 @@ namespace fpWebApp
 {
     public class clasesglobales
     {
+        #region Otros
+
+        public DataTable cargarPlanesAfiliado(string idAfiliado)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_CARGAR_PLANES_AFILIADO", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_id_afiliado", idAfiliado);
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public DataTable ValidarPermisos(string strPagina, string idPerfil, string idUsuario)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_VALIDAR_PERMISOS", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_pagina", strPagina);
+                        cmd.Parameters.AddWithValue("@p_id_perfil", Convert.ToInt32(idPerfil));
+                        cmd.Parameters.AddWithValue("@p_id_usuario", Convert.ToInt32(idUsuario));
+
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public DataTable TraerDatos(string strQuery)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(strQuery, mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+
+            return dt;
+        }
+
+        public void InsertarLog(string idUsuario, string tabla, string accion, string descripcion, string datosAnteriores, string datosNuevos)
+        {
+            // Tarea de Javier
+            OdbcConnection myConnection = new OdbcConnection(ConfigurationManager.AppSettings["sConn"].ToString());
+            try
+            {
+                string strQuery = "INSERT INTO logs " +
+                    "(idUsuario, FechaHora, Tabla, Accion, DatosAnteriores, DatosNuevos, DescripcionLog) " +
+                    "VALUES (" + idUsuario + ", now(), '" + tabla + "', '" + accion + "', '" + datosAnteriores + "', '" + datosNuevos + "', '" + descripcion + "') ";
+                //"VALUES (" + idUsuario + ", DATE_SUB(NOW(), INTERVAL 5 HOUR), '" + tabla + "', '" + accion + "', '" + datosAnteriores + "', '" + datosNuevos + "', '" + descripcion + "') ";
+                OdbcCommand command = new OdbcCommand(strQuery, myConnection);
+                myConnection.Open();
+                command.ExecuteNonQuery();
+                command.Dispose();
+                myConnection.Close();
+            }
+            catch (OdbcException ex)
+            {
+                string mensaje = ex.Message;
+                myConnection.Close();
+            }
+        }
+
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghkmnpqrstuvwxyzABCDEFGHKMNPQRSTUVWXYZ123456789*%$_-@#&?";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                var unused = res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        public void EnviarCorreo(string strRemitente, string strDestinatario, string strAsunto, string strMensaje)
+        {
+            MailMessage objeto_mail = new MailMessage();
+            objeto_mail.From = new MailAddress(strRemitente);
+            MailAddress maTo = new MailAddress(strDestinatario);
+            objeto_mail.To.Add(maTo);
+            objeto_mail.Subject = strAsunto;
+            objeto_mail.Body = strMensaje;
+
+            SmtpClient client = new SmtpClient();
+            client.Host = "localhost";
+            client.Port = 25;
+            client.UseDefaultCredentials = false;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new System.Net.NetworkCredential("contabilidad@fitnesspeoplecolombia.com", "K)961558128719os");
+
+            try
+            {
+                client.Send(objeto_mail);
+                objeto_mail.Dispose();
+            }
+            catch (Exception ex)
+            {
+                string strError = ex.Message;
+            }
+
+        }
+
+        #endregion
 
         #region Ciudades
         public DataTable ConsultarCiudades()
@@ -1567,6 +1728,7 @@ namespace fpWebApp
 
             return dt;
         }
+        #endregion
 
         #region Ciudades sedes
 
