feacd4b0104d555b5ffd37c4cb30614484f707fa
javiergalvanmancera <javier.galvan.mancera@gmail.com>
2025-02-18 06:13:25 -0500

    JG Ajustes columnas formulario sedes 17/02/2025 5;00pm

--- sedes.aspx.designer.cs
+++ sedes.aspx.designer.cs
@@ -87,13 +87,13 @@ namespace fpWebApp
         protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
-        /// Control UpdatePanel1.
+        /// Control upSedes.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        protected global::System.Web.UI.UpdatePanel upSedes;
 
         /// <summary>
         /// Control txbSede.
