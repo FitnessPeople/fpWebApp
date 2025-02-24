5378be70c6a3e8522f644fc080313fac53703841
javiergalvanmancera <javier.galvan.mancera@gmail.com>
2025-02-24 06:29:12 -0500

    JG On stage

--- clasesglobales.cs
+++ clasesglobales.cs
@@ -4179,6 +4179,35 @@ namespace fpWebApp
                 dt.Rows.Add(ex.Message);
             }
 
+        public DataTable ConsultarPagosWompiPorId(int idPago)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_CONSULTAR_PAGOS_TRANSAC_ID", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_id_pago", idPago);
+
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+
             return dt;
         }
 
