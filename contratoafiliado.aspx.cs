0b862941c34441eb62ab2e50127b154d54525b22
Christian Morales <sistemas@fitnesspeoplecmd.com>
2025-03-14 08:37:49 -0500

    CM Cambios en ContratoAfiliado

--- contratoafiliado.aspx.cs
+++ contratoafiliado.aspx.cs
@@ -21,6 +21,39 @@ namespace fpWebApp
                         //No tiene acceso a esta página
                         divMensaje.Visible = true;
                         paginasperfil.Visible = true;
+                        divContenido.Visible = false;
+                    }
+                    else
+                    {
+                        //Si tiene acceso a esta página
+                        btnAgregar.Visible = false;
+                        if (ViewState["Consulta"].ToString() == "1")
+                        {
+                            
+                        }
+                        if (ViewState["Exportar"].ToString() == "1")
+                        {
+                            
+                        }
+                        if (ViewState["CrearModificar"].ToString() == "1")
+                        {
+                            btnAgregar.Visible = true;
+                        }
+                    }
+
+                    string strQuery = "SELECT * " +
+                        "FROM EmpresasFP " +
+                        "WHERE idEmpresaFP = 1 ";
+            if (!IsPostBack)
+            {
+                if (Session["idUsuario"] != null)
+                {
+                    ValidarPermisos("Sedes");
+                    if (ViewState["SinPermiso"].ToString() == "1")
+                    {
+                        //No tiene acceso a esta página
+                        divMensaje.Visible = true;
+                        paginasperfil.Visible = true;
                     }
                     else
                     {
@@ -44,21 +77,29 @@ namespace fpWebApp
                 "FROM EmpresasFP " +
                 "WHERE idEmpresaFP = 1 ";
 
-            clasesglobales cg = new clasesglobales();
-            DataTable dt = cg.TraerDatos(strQuery);
+                    clasesglobales cg = new clasesglobales();
+                    DataTable dt = cg.TraerDatos(strQuery);
 
-            string strTextoContrato = dt.Rows[0]["ContratoMayorEdad"].ToString();
-            string contenidoEditor = hiddenEditor.Value;
-            hiddenEditor.Value = dt.Rows[0]["ContratoMayorEdad"].ToString();
+                    string strTextoContrato = dt.Rows[0]["ContratoMayorEdad"].ToString();
+                    string contenidoEditor = hiddenEditor.Value;
+                    hiddenEditor.Value = dt.Rows[0]["ContratoMayorEdad"].ToString();
 
-            strQuery = "SELECT * " +
-                "FROM Afiliados " +
-                "WHERE idAfiliado = 10036 ";
+                    strQuery = "SELECT * " +
+                        "FROM Afiliados " +
+                        "WHERE idAfiliado = 10036 ";
 
-            DataTable dt1 = cg.TraerDatos(strQuery);
+                    DataTable dt1 = cg.TraerDatos(strQuery);
 
-            strTextoContrato = strTextoContrato.Replace("#NOMBRE#", dt1.Rows[0]["NombreAfiliado"].ToString() + " " + dt1.Rows[0]["ApellidoAfiliado"].ToString());
+                    strTextoContrato = strTextoContrato.Replace("#NOMBRE#", dt1.Rows[0]["NombreAfiliado"].ToString() + " " + dt1.Rows[0]["ApellidoAfiliado"].ToString());
 
+                    ltContrato.Text = strTextoContrato;
+                }
+                else
+                {
+                    Response.Redirect("logout");
+                }
+            }
+        }
                     ltContrato.Text = strTextoContrato;
                 }
                 else
@@ -77,12 +118,29 @@ namespace fpWebApp
             ViewState["Exportar"] = "0";
             ViewState["CrearModificar"] = "0";
             ViewState["Borrar"] = "0";
+        private void ValidarPermisos(string strPagina)
+        {
+            ViewState["SinPermiso"] = "1";
+            ViewState["Consulta"] = "0";
+            ViewState["Exportar"] = "0";
+            ViewState["CrearModificar"] = "0";
+            ViewState["Borrar"] = "0";
             //Traer los datos del afiliado en un dt
 
             clasesglobales cg = new clasesglobales();
             DataTable dt = cg.ValidarPermisos(strPagina, Session["idPerfil"].ToString(), Session["idusuario"].ToString());
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.ValidarPermisos(strPagina, Session["idPerfil"].ToString(), Session["idusuario"].ToString());
             // Llenar los campos del texto del contrato con los datos del dt del afiliado.
 
+            if (dt.Rows.Count > 0)
+            {
+                ViewState["SinPermiso"] = dt.Rows[0]["SinPermiso"].ToString();
+                ViewState["Consulta"] = dt.Rows[0]["Consulta"].ToString();
+                ViewState["Exportar"] = dt.Rows[0]["Exportar"].ToString();
+                ViewState["CrearModificar"] = dt.Rows[0]["CrearModificar"].ToString();
+                ViewState["Borrar"] = dt.Rows[0]["Borrar"].ToString();
+            }
             if (dt.Rows.Count > 0)
             {
                 ViewState["SinPermiso"] = dt.Rows[0]["SinPermiso"].ToString();
@@ -103,6 +161,7 @@ namespace fpWebApp
 
             try
             {
+                string respuesta = cg.TraerDatosStr("UPDATE EmpresasFP SET ContratoMayorEdad = '" + contenidoEditor + "' WHERE idEmpresaFP = 1 ");
                 string respuesta = cg.TraerDatosStr("UPDATE EmpresasFP SET ContratoMayorEdad = '" + contenidoEditor + "' WHERE idEmpresaFP = 1 ");
                 string respuesta = cg.TraerDatosStr("UPDATE EmpresasFP" + contenidoEditor);
                 //cg.InsertarLog(Session["idusuario"].ToString(), "sedes", "Agrega", "El usuario agregó una nueva sede: " + txbSede.Text.ToString() + ".", "", "");
