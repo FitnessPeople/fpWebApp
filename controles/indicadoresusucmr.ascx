e3bf0f4f8fc034a41066ced617bbea5d57d44c2b
desarrollofitness <desarrollo@fitnesspeoplecmd.com>
2025-04-07 11:48:57 -0500

    JG RM indicadores 07/04/2025

---
+++ controles/indicadoresusucmr.ascx
@@ -0,0 +1,73 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="indicadoresusucmr.ascx.cs" Inherits="fpWebApp.controles.indicadoresusucmr" %>
+
+
+<%--
+    ****************
+    Indicadores: 
+        Primer contacto
+        Propuesta enviada
+        Negociación propuesta
+        Negociación aceptada
+        Negociación rechazada
+    ****************
+--%>
+<div class="row">
+    <div class="col-lg-3">
+        <div class="widget style1 lazur-bg ">
+            <div class="row">
+                <div class="col-xs-4 text-center">
+                <i class="fa-solid fa-hand-point-up fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                    <span>Primer contacto</span>
+                    <h2 class="font-bold">
+                        <asp:Literal ID="ltCuantos1" runat="server"></asp:Literal></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="widget style1 bg-success">
+            <div class="row">
+                <div class="col-xs-4">
+                    <i class="fa-solid fa-paper-plane fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                    <span>Propuesta enviada</span>
+                    <h2 class="font-bold">
+                        <asp:Literal ID="ltCuantos2" runat="server"></asp:Literal>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="widget style1 yellow-bg">
+            <div class="row">
+                <div class="col-xs-4">
+                    <i class="fa-regular fa-file-lines fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                    <span>Negociación propuesta</span>
+                    <h2 class="font-bold">
+                        <asp:Literal ID="ltCuantos3" runat="server"></asp:Literal>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="widget style1 navy-bg">
+            <div class="row">
+                <div class="col-xs-4">
+                    <i class="fa-solid fa-handshake fa-5x"></i>
+                </div>
+                <div class="col-xs-8 text-right">
+                    <span>Negociación aceptada </span>
+                    <h2 class="font-bold">
+                        <asp:Literal ID="ltCuantos4" runat="server"></asp:Literal></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ controles/indicadoresusucmr.ascx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fpWebApp.controles
+{
+    public partial class indicadoresusucmr : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidarPermisos("Especialistas");
+            if (ViewState["SinPermiso"].ToString() == "0")
+            {
+                CuantosEspecialistasActivos();
+                CuantosEspecialistasInactivos();
+                CuantasEspecialidades();
+                CuantasCitasHoy();
+            }
+        }
+
+        private void ValidarPermisos(string strPagina)
+        {
+            ViewState["SinPermiso"] = "1";
+            ViewState["Consulta"] = "0";
+            ViewState["Exportar"] = "0";
+            ViewState["CrearModificar"] = "0";
+            ViewState["Borrar"] = "0";
+
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.ValidarPermisos(strPagina, Session["idPerfil"].ToString(), Session["idusuario"].ToString());
+
+            if (dt.Rows.Count > 0)
+            {
+                ViewState["SinPermiso"] = dt.Rows[0]["SinPermiso"].ToString();
+                ViewState["Consulta"] = dt.Rows[0]["Consulta"].ToString();
+                ViewState["Exportar"] = dt.Rows[0]["Exportar"].ToString();
+                ViewState["CrearModificar"] = dt.Rows[0]["CrearModificar"].ToString();
+                ViewState["Borrar"] = dt.Rows[0]["Borrar"].ToString();
+            }
+
+            dt.Dispose();
+        }
+
+        private void CuantosEspecialistasActivos()
+        {
+            string strQuery = "SELECT COUNT(*) AS cuantos FROM contactoscrm WHERE idEstadoCRM = 1 ";
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.TraerDatos(strQuery);
+
+            ltCuantos1.Text = dt.Rows[0]["cuantos"].ToString();
+
+            dt.Dispose();
+        }
+
+        private void CuantosEspecialistasInactivos()
+        {
+            string strQuery = "SELECT COUNT(*) AS cuantos FROM contactoscrm WHERE idEstadoCRM = 2 ";
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.TraerDatos(strQuery);
+
+            ltCuantos2.Text = dt.Rows[0]["cuantos"].ToString();
+
+            dt.Dispose();
+        }
+
+        private void CuantasEspecialidades()
+        {
+            string strQuery = "SELECT COUNT(*) AS cuantos FROM contactoscrm WHERE idEstadoCRM = 3 ";
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.TraerDatos(strQuery);
+
+            ltCuantos3.Text = dt.Rows[0]["cuantos"].ToString();
+
+            dt.Dispose();
+        }
+
+        private void CuantasCitasHoy()
+        {
+            string strQuery = "SELECT COUNT(*) AS cuantos FROM contactoscrm WHERE idEstadoCRM = 4 ";
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.TraerDatos(strQuery);
+
+            //ltCuantos4.Text = dt.Rows[0]["cuantos"].ToString();
+            ltCuantos4.Text = "15";
+
+            dt.Dispose();
+        }
+    }
+}
