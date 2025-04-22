fa20c828a9ea16dc358b95318dd597b28e2b2f6d
javiergalvanmancera <javier.galvan.mancera@gmail.com>
2025-04-21 23:11:49 -0500

    JG Cambio de formato a historialHTML2 en formulario ListacontactosCRM 21/04/2025 2111

--- listacontactoscrm.aspx.cs
+++ listacontactoscrm.aspx.cs
@@ -135,6 +135,8 @@ namespace fpWebApp
 
             ddlEmpresa.DataSource = dt;
             ddlEmpresa.DataBind();
+            rpEmpresaCRM.DataSource = dt;
+            rpEmpresaCRM.DataBind();
             dt.Dispose();
         }
 
