a7d5906c27a09ebae7a8293c4cad76b15f56b89c
FullStackFitnessPeople <fullstack@fitnesspeoplecmd.com>
2025-05-21 11:59:06 -0500

    CR Terminando el agendamiento de Gym Pass

--- clasesglobales.cs
+++ clasesglobales.cs
@@ -6975,6 +6975,37 @@ namespace fpWebApp
             return dt;
         }
 
+        public DataTable ConsultarGymPassAgendaYGymPassPorSede(int idSede)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_CONSULTAR_GYM_PASS_AGENDA_Y_GYM_PASS_POR_SEDE", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_id_sede", idSede);
+
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+            return dt;
+        }
+
         #endregion
 
     }
