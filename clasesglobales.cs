c7c15b08c0ae3a1ecef2a3ab9b417aa0e13926e1
FullStackFitnessPeople <fullstack@fitnesspeoplecmd.com>
2025-05-27 16:56:27 -0500

    CR Modificaciones y arreglos de permisos para los usuarios en Gym Pass

--- clasesglobales.cs
+++ clasesglobales.cs
@@ -2864,6 +2864,38 @@ namespace fpWebApp
             return dt;
         }
 
+        public DataTable ConsultaCargarSedesPorId(int? idSede, string p_clase_sede)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_CONSULTAR_CARGAR_SEDES_POR_ID", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@p_id_sede", idSede.HasValue ? idSede.Value : (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@p_clase_sede", p_clase_sede);// Gimnasio / Oficina / Todos 
+
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+            return dt;
+        }
+
         public DataTable ConsultaCargarSedes(string p_clase_sede)
         {
             DataTable dt = new DataTable();
