d5fa4345c98a3a6b5c27582ce0cdd8fbb2391ced
desarrollofitness <desarrollo@fitnesspeoplecmd.com>
2025-09-09 11:37:50 -0500

    JG consultas en control ascx

--- controles/indicadoresusucrm2.ascx.cs
+++ controles/indicadoresusucrm2.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,53 @@ namespace fpWebApp.controles
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ConsultarContactosActivosPorUsuario();
 
+        }
+
+        private void ConsultarContactosActivosPorUsuario()
+        {
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.ConsultarMetasComerciales();
+            DataTable dt1 = cg.ConsultarCanalesVenta();
+            DateTime hoy = DateTime.Today;
+
+            DataRow[] filasFiltradas = dt.Select("idCanalVenta <> 1"); // Se excluye la opción 1 Ninguno
+
+            if (filasFiltradas.Length > 0)
+            {
+                dt = filasFiltradas.CopyToDataTable();
+            }
+            else
+            {
+                dt.Clear();
+            }
+
+        }
+
+        private void ListaCanalesDeVenta()
+        {
+            clasesglobales cg = new clasesglobales();
+            DataTable dt = cg.ConsultarCanalesVenta();
+
+
+            DataRow[] filasFiltradas = dt.Select("idCanalVenta <> 1"); // Se excluye la opción 1 Ninguno
+
+            if (filasFiltradas.Length > 0)
+            {
+                dt = filasFiltradas.CopyToDataTable();
+            }
+            else
+            {
+                dt.Clear();
+            }
+
+            chblCanales.DataSource = dt;
+            chblCanales.DataTextField = "NombreCanalVenta";
+            chblCanales.DataValueField = "idCanalVenta";
+            chblCanales.DataBind();
+
+            dt.Dispose();
         }
     }
 }
