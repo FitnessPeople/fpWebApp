c3f37d871baaf785d595814c3eaad1f361c62629
FullStackFitnessPeople <fullstack@fitnesspeoplecmd.com>
2025-09-15 11:08:11 -0500

    CR Eliminación de permisos en indicadorconcursogympass

--- controles/indicadoresconcursogympass.ascx.cs
+++ controles/indicadoresconcursogympass.ascx.cs
@@ -12,37 +12,10 @@ namespace fpWebApp.controles
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ValidarPermisos("Páginas");
-            if (ViewState["SinPermiso"].ToString() == "0")
-            {
-                CantidadTotalPersonasRegistradas();
-                CantidadEmbajadorCodigosMasRegistrados();
-                CantidadSedesMasPersonasRegistradas();
-                FechaMasPersonasRegistradas();
-            }
-        }
-
-        private void ValidarPermisos(string strPagina)
-        {
-            ViewState["SinPermiso"] = "1";
-            ViewState["Consulta"] = "0";
-            ViewState["Exportar"] = "0";
-            ViewState["CrearModificar"] = "0";
-            ViewState["Borrar"] = "0";
-
-            clasesglobales cg = new clasesglobales();
-            DataTable dt = cg.ValidarPermisos(strPagina, Session["idPerfil"].ToString(), Session["idusuario"].ToString());
-
-            if (dt.Rows.Count > 0)
-            {
-                ViewState["SinPermiso"] = dt.Rows[0]["SinPermiso"].ToString();
-                ViewState["Consulta"] = dt.Rows[0]["Consulta"].ToString();
-                ViewState["Exportar"] = dt.Rows[0]["Exportar"].ToString();
-                ViewState["CrearModificar"] = dt.Rows[0]["CrearModificar"].ToString();
-                ViewState["Borrar"] = dt.Rows[0]["Borrar"].ToString();
-            }
-
-            dt.Dispose();
+            CantidadTotalPersonasRegistradas();
+            CantidadEmbajadorCodigosMasRegistrados();
+            CantidadSedesMasPersonasRegistradas();
+            FechaMasPersonasRegistradas();
         }
 
         private void CantidadTotalPersonasRegistradas()
