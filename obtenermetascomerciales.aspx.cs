793525b1af68842e772c128205a0e4a22e10c7cf
desarrollofitness <desarrollo@fitnesspeoplecmd.com>
2025-09-17 08:46:57 -0500

    JG Obtenermestascomerciales para estacionalidad

--- obtenermetascomerciales.aspx.cs
+++ obtenermetascomerciales.aspx.cs
@@ -9,6 +9,9 @@ namespace fpWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //DateTime hoy = DateTime.Today;
+            //int mes = hoy.Month;
+            //int anio = hoy.Year;
             string filtro = Request.QueryString["filtro"];
 
             if (filtro == "" || filtro is null)
@@ -16,32 +19,26 @@ namespace fpWebApp
                 filtro = Session["idCanalVenta"].ToString();
             }
 
-            //string strQuery = "SELECT *, (mc.Valor * e.Titulo / 100) metaDia " +
-            //    "FROM estacionalidad e, metascomerciales mc " +
-            //    "WHERE mc.idCanalVenta = " + filtro + " " +
-            //    "AND mc.mes = " + Request.QueryString["mes"].ToString() + " " +
+            //string strQuery = "SELECT e.FechaInicio, e.FechaFin, " +
+            //    "e.idEstacionalidad, e.Titulo, e.Renderizado, e.Color, e.TodoElDia, e.Mostrar, " +
+            //    "(mc.Presupuesto * e.Titulo / 100) AS metaDia, " +
+            //    "IF(SUM(ppa.Valor) IS NULL, 0, SUM(ppa.Valor)) AS pagado, mc.Valor  " +
+            //    "FROM estacionalidad e " +
+            //    "INNER JOIN metascomerciales mc " +
+            //    "ON mc.mes = " + Request.QueryString["mes"].ToString() + " " +
             //    "AND mc.annio = " + Request.QueryString["anio"].ToString() + " " +
-            //    "AND MONTH(e.FechaInicio) = " + Request.QueryString["mes"].ToString() + " " +
-            //    "AND YEAR(e.FechaInicio) = " + Request.QueryString["anio"].ToString() + " ";
-
-            string strQuery = "SELECT e.FechaInicio, e.FechaFin, " +
-                "e.idEstacionalidad, e.Titulo, e.Renderizado, e.Color, e.TodoElDia, e.Mostrar, " +
-                "(mc.Presupuesto * e.Titulo / 100) AS metaDia, " +
-                "IF(SUM(ppa.Valor) IS NULL, 0, SUM(ppa.Valor)) AS pagado, mc.Valor  " +
-                "FROM estacionalidad e " +
-                "INNER JOIN metascomerciales mc " +
-                "ON mc.mes = " + Request.QueryString["mes"].ToString() + " " +
-                "AND mc.annio = " + Request.QueryString["anio"].ToString() + " " +
-                "AND mc.idCanalVenta = 9 " +
-                "LEFT JOIN pagosplanafiliado ppa " +
-                "ON DATE(ppa.FechaHoraPago) = e.FechaInicio " +
-                "WHERE MONTH(e.FechaInicio) = " + Request.QueryString["mes"].ToString() + " " +
-                "AND YEAR(e.FechaInicio) = " + Request.QueryString["anio"].ToString() + " " +
-                "GROUP BY e.FechaInicio, (mc.Valor * e.Titulo / 100), e.Titulo, e.idEstacionalidad, " +
-                "e.FechaFin, e.Renderizado, e.Color, e.TodoElDia, e.Mostrar, mc.Presupuesto ";
+            //    "AND mc.idCanalVenta = 9 " +
+            //    "LEFT JOIN pagosplanafiliado ppa " +
+            //    "ON DATE(ppa.FechaHoraPago) = e.FechaInicio " +
+            //    "WHERE MONTH(e.FechaInicio) = " + Request.QueryString["mes"].ToString() + " " +
+            //    "AND YEAR(e.FechaInicio) = " + Request.QueryString["anio"].ToString() + " " +
+            //    "GROUP BY e.FechaInicio, (mc.Valor * e.Titulo / 100), e.Titulo, e.idEstacionalidad, " +
+            //    "e.FechaFin, e.Renderizado, e.Color, e.TodoElDia, e.Mostrar, mc.Presupuesto ";
 
             clasesglobales cg = new clasesglobales();
-            DataTable dt = cg.TraerDatos(strQuery);
+
+            DataTable dt = cg.ConsultarEstacionalidadPorDia(Convert.ToInt32(filtro),Convert.ToInt32(Request.QueryString["mes"].ToString()),Convert.ToInt32(Request.QueryString["anio"].ToString()));
+
 
             var lista = new List<object>();
             foreach (DataRow row in dt.Rows)
@@ -49,7 +46,7 @@ namespace fpWebApp
                 int metaDia = Convert.ToInt32(row["metaDia"]);
                 //int pagado = (row["pagado"] == DBNull.Value || row["pagado"] == null) ? 0 : Convert.ToInt32(row["pagado"]);
                 int pagado = Convert.ToInt32(row["pagado"]);
-                int intDiferencia = Convert.ToInt32(row["metaDia"]) - pagado;
+                int intDiferencia = Convert.ToInt32(row["metaSedeDia"]) - pagado;
                 decimal dblCumplimiento = metaDia > 0 ? ((decimal)pagado / metaDia) * 100 : 0;
 
                 string strColor = "";
@@ -63,10 +60,10 @@ namespace fpWebApp
                 lista.Add(new
                 {
                     id = row["idEstacionalidad"],
-                    title = "Meta: $ " + String.Format("{0:N0}", row["metaDia"]),
+                    title = "Meta: $ " + String.Format("{0:N0}", row["metaSedeDia"]),
                     valor = row["Valor"],
                     ventas = Convert.ToInt32(row["pagado"]),
-                    description = "Meta: $ " + String.Format("{0:N0}", row["metaDia"]) + "\r\n" +
+                    description = "Meta: $ " + String.Format("{0:N0}", row["metaSedeDia"]) + "\r\n" +
                     "Ventas: $ " + String.Format("{0:N0}", pagado) + "\r\n" +
                     "Diferencia: $ " + String.Format("{0:N0}", intDiferencia) + "\r\n" +
                     "Cumplimiento: " + String.Format("{0:N0}", dblCumplimiento) + "%",
