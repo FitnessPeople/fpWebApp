0f01c123be6838dba391d748f74a8463e53ac973
desarrollofitness <desarrollo@fitnesspeoplecmd.com>
2025-09-22 10:45:13 -0500

    JG Grafica usucrm

--- controles/indicadoresusucrm2.ascx.designer.cs
+++ controles/indicadoresusucrm2.ascx.designer.cs
@@ -15,6 +15,15 @@ namespace fpWebApp.controles
     {
 
         /// <summary>
+        /// Control ltFechaHoy.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltFechaHoy;
+
+        /// <summary>
         /// Control ltValorMetaAsesorHoy.
         /// </summary>
         /// <remarks>
