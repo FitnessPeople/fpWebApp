791fce36b8e214fd7decb8c971b124f7daf76919
desarrollofitness <desarrollo@fitnesspeoplecmd.com>
2025-11-07 12:11:37 -0500

    JG Consultar efectividad

--- clasesglobales.cs
+++ clasesglobales.cs
@@ -5733,6 +5733,35 @@ namespace fpWebApp
             return dt;
         }
 
+        public DataTable ConsultaCargarAsesoresActivos()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string strConexion = WebConfigurationManager.ConnectionStrings["ConnectionFP"].ConnectionString;
+                using (MySqlConnection mysqlConexion = new MySqlConnection(strConexion))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("Pa_CARGAR_ASESORES_ACTIVOS", mysqlConexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd))
+                        {
+                            mysqlConexion.Open();
+                            dataAdapter.Fill(dt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Error", typeof(string));
+                dt.Rows.Add(ex.Message);
+            }
+
+            return dt;
+        }
         #endregion
 
         #region Congelaciones
