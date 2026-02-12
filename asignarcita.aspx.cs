ddc32bdd116dea391aa44ffc84a8cba6844063a2
SistemasFitness <sistemas@fitnesspeoplecmd.com>
2026-02-12 14:45:25 -0500

    CM Modificaciones: AsignarCita

--- asignarcita.aspx.cs
+++ asignarcita.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra.Complex.Solvers;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -19,21 +20,51 @@ namespace fpWebApp
                     }
                     if (ViewState["Borrar"].ToString() == "1")
                     {
+                        clasesglobales cg = new clasesglobales();
                         try
                         {
                             string strQuery = "UPDATE DisponibilidadEspecialistas SET " +
                                 "idAfiliado = " + Request.QueryString["idAfil"].ToString() + ", " +
                                 "idUsuarioAsigna = " + Session["idusuario"].ToString() + " " +
                                 "WHERE idDisponibilidad = " + Request.QueryString["id"].ToString();
-                            clasesglobales cg = new clasesglobales();
+                            
                             string mensaje = cg.TraerDatosStr(strQuery);
+
+                            //Creamos el concepto de pago en la tabla PagosAdicionalesAfiliado
+                            strQuery = "INSERT INTO PagosAdicionalesAfiliado (" +
+                                "idPagoAdicional, idAfiliado, Valor, Cantidad, EstadoPago) " +
+                                "VALUES (1, " + Request.QueryString["idAfil"].ToString() + ", 50000, 1, 'Pendiente')";
+
+                            DataTable dtAfiliado = cg.ConsultarAfiliadoPorId(Convert.ToInt32(Request.QueryString["idAfil"].ToString()));
+
+                            //Enviar correo al afiliado con la cita y el enlace de pago.
+                            DataTable dtDisponibilidad = cg.TraerDatos(@"SELECT * 
+                                FROM DisponibilidadEspecialistas 
+                                WHERE idDisponibilidad = " + Request.QueryString["id"].ToString());
+
+                            string fecha = String.Format("{0:yyyy-MM-dd}", dtDisponibilidad.Rows[0]["FechaHoraInicio"]);
+                            string hora = String.Format("{0:HH:mm:ss}", dtDisponibilidad.Rows[0]["FechaHoraInicio"]);
+                            string enlacePago = "https://fitnesspeoplecmdcolombia.com";
+
+                            string mensajeCorreo =
+                                "Estimado(a) afiliado(a)," + Environment.NewLine + Environment.NewLine +
+                                "Reciba un cordial saludo." + Environment.NewLine + Environment.NewLine +
+                                "Le confirmamos que su cita ha sido agendada exitosamente con el siguiente detalle:" + Environment.NewLine + Environment.NewLine +
+                                "Fecha: " + fecha + Environment.NewLine +
+                                "Hora: " + hora + Environment.NewLine + Environment.NewLine +
+                                "Para confirmar definitivamente su cita, es necesario realizar el pago correspondiente a través del siguiente enlace:" + Environment.NewLine + Environment.NewLine +
+                                "Enlace de pago: " + enlacePago + Environment.NewLine + Environment.NewLine +
+                                "Le recomendamos efectuar el pago lo antes posible para garantizar la reserva de su espacio. Una vez realizado, recibirá la confirmación automática de su cita." + Environment.NewLine + Environment.NewLine +
+                                "Si tiene alguna inquietud, puede responder a este correo o comunicarse con nuestro equipo de soporte." + Environment.NewLine + Environment.NewLine +
+                                "Cordialmente," + Environment.NewLine +
+                                "Equipo Fitness People CMD";
+
+                            cg.EnviarCorreo("sistemas@fitnesspeoplecmd.com", dtAfiliado.Rows[0]["EmailAfiliado"].ToString(), "Cita agendada", mensajeCorreo);
                         }
                         catch (SqlException ex)
                         {
                             string mensaje = ex.Message;
                         }
-                        
-                        //FALTA Enviar Correo al Afiliado con la cita.
                     }
                     Response.Redirect("agendarcita");
                 }
